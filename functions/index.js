8ee1514b2fcdf122985bcb0e1bcb7ad92d8671d5
barbabianca <ukasikcioglu@example.com>
2023-05-13 01:17:04 +0530

    formdatasini_maillebildiriyor

--- functions/index.js
+++ functions/index.js
@@ -18,38 +18,47 @@
 //   response.send("Hello from Firebase!");
 // });
 
-const functions = require('firebase-functions')
-const admin = require("firebase-admin")
+const functions = require('firebase-functions');
+const admin = require("firebase-admin");
 const nodemailer = require('nodemailer');
-admin.initializeApp()
-/* gmail  credentials */
-var transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'ukasikcioglu@gmail.com',
-        pass: 'iovxglahevthrqzu'
-    }
+const cors = require('cors');
+
+admin.initializeApp();
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'ukasikcioglu@gmail.com',
+    pass: 'iovxglahevthrqzu'
+  }
 });
+
+// Enable CORS for your function
 exports.sendMailOverHTTP = functions.https.onRequest((req, res) => {
+  // Use cors middleware to allow requests from localhost during development
+  cors()(req, res, () => {
     const mailOptions = {
-        from: `YourGmailEmailAddress@YouWant.com`,
-        to: `YourGmailEmailAddress@gmail.com`,
-        subject: 'Email From Me to MySelf | Contact Form Message',
-        html: `<h1>Contact Form Message</h1>
-                            <p>
-                               <b>Email: </b>${req.body.email}<br>
-                               <b>Name: </b>${req.body.name}<br>
-                                <b>Mobile: </b>${req.body.name}<br>
-                               <b>Message: </b>${req.body.message}<br>
-                            </p>`
+      from: `ukasikcioglu@YouWant.com`,
+      to: `ukasikcioglu@gmail.com`,
+      subject: 'Email From Me to MySelf | Contact Form Message',
+      html: `<h1>Contact Form Message</h1>
+              <p>
+                <b>Email: </b>${req.body.email}<br>
+                <b>Name: </b>${req.body.name}<br>
+                <b>Subject: </b>${req.body.subject}<br>
+                <b>Message: </b>${req.body.message}<br>
+              </p>`
     };
+
     return transporter.sendMail(mailOptions, (error, data) => {
-        if (error) {
-            return res.send(error.toString());
-        }
-        var data = JSON.stringify(data)
-        return res.send(`Sent! ${data}`);
+      if (error) {
+        return res.send(error.toString());
+      }
+
+      const responseData = JSON.stringify(data);
+      return res.send(`Sent! ${responseData}`);
     });
+  });
 });
